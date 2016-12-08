389bbc79a05e4179f2f3f8968bb8b2d92172a606
michaeledwards91 <michael.edwards1991@gmail.com>
2016-12-07 18:04:36 -0600

    Added object to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,9 @@
 var friendsArray = [
 	{
-		
+		name: "Dog",
+		photo: "http://www.memecreator.org/static/images/templates/49.jpg",
+		scores: ["1", "5", "3", "4", "2", "2", "1", "4", "1", "5"]
 	}
 ];
+
+module.exports = friendsArray;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,10 +1,10 @@
-
+var friendsArray = require("../data/friends.js");
 
 module.exports = function (app) {
 
 	app.get("/api/friends", function(req, res) {
 		console.log(req);
-		res.json("5555555");
+		res.json(friendsArray);
 	});
 
 	app.post("/api/friends", function(req, res) {
