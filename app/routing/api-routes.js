18f5106871437c6f8e998e3e1a14c1290ff41f73
michaeledwards91 <michael.edwards1991@gmail.com>
2016-12-08 21:03:45 -0600

    Implemented logic for finding best match and displaying best match info in modal.

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -9,7 +9,35 @@ module.exports = function (app) {
 	app.post("/api/friends", function(req, res) {
 		console.log(req.body);
 
+		var lowestDiff = 1000;
+		var bestMatch = friendsArray[0];
+		//Function to calculate absolute difference of 2 numbers for comparing scores array indexes
+		function absDiff(num1, num2) {
+		  if (num1 > num2) {
+		    return num1 - num2;
+		  } else if (num2 > num1) {
+		    return num2 - num1;
+		  } else {
+		    return 0;
+		  }
+		}
+		//For each item in friends array, i want to compare req.body's scores array with current friend's score's array
+		for (var i = 0; i < friendsArray.length; i++) {
+		  var currentFriendDiff = 0;
+		  for (var t = 0; t < friendsArray[i].scores.length; t++) {
+		    currentFriendDiff += absDiff(friendsArray[i].scores[t], req.body.scores[t]);
+		  }
+		  console.log("current friend diff: " + currentFriendDiff);
+		  console.log("lowest diff: " + lowestDiff);
+		  if (currentFriendDiff < lowestDiff && req.body.name != friendsArray[i].name) {
+		    lowestDiff = currentFriendDiff;
+		    bestMatch = friendsArray[i];
+		  }
+		  console.log("best match name:" + bestMatch.name);
+		}
+		//Send best match back
+		res.send(bestMatch);
+
 		friendsArray.push(req.body);
-		res.send("hello");
 	});
 }
